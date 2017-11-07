caa8425218a8ff3922a484305589bbd41041d4a4
MateuszKubuszok <mateusz.kubuszok@gmail.com>
2017-11-07 12:19:36 +0100

    Made ProductProvider non-memoized

--- pulp/src/main/scala/io/scalaland/pulp/semiauto/ProductProvider.scala
+++ pulp/src/main/scala/io/scalaland/pulp/semiauto/ProductProvider.scala
@@ -1,5 +1,11 @@
 package io.scalaland.pulp.semiauto
 
-import shapeless.HList
+import io.scalaland.pulp.Provider
+import shapeless.{::, HList}
 
-final class ProductProvider[A <: HList](val get: A) extends AnyVal
+final class ProductProvider[A <: HList](thunk: => A) {
+
+  def get: A = thunk
+
+  def ::[B](provider: Provider[B]): ProductProvider[B :: A] = new ProductProvider(provider.get :: get)
+}
--- pulp/src/main/scala/io/scalaland/pulp/semiauto/ProductProviderInstances.scala
+++ pulp/src/main/scala/io/scalaland/pulp/semiauto/ProductProviderInstances.scala
@@ -6,10 +6,10 @@ import shapeless.{::, Generic, HList, HNil}
 trait ProductProviderInstances {
 
   implicit def product[A, AG <: HList](implicit aRepr: Generic.Aux[A, AG], agP: ProductProvider[AG]): Provider[A] =
-    Provider.const(aRepr.from(agP.get))
+    Provider.factory(aRepr.from(agP.get))
 
   implicit val hnil: ProductProvider[HNil] = new ProductProvider(HNil)
 
   implicit def hcons[H, T <: HList](implicit headP: Provider[H], tailP: ProductProvider[T]): ProductProvider[H :: T] =
-    new ProductProvider(headP.get :: tailP.get)
+    headP :: tailP
 }
